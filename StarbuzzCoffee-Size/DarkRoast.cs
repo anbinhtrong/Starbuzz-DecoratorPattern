f07d3361c1a61734a3ef8559d1162a8d0a6a0850
Án Bình Trọng <minhtam.seta@gmail.com>
2014-02-08 23:05:11 +0700

    Add size to Soy

---
+++ StarbuzzCoffee-Size/DarkRoast.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Starbuzz_DecoratorPattern
+{
+    public class DarkRoast : Beverage
+    {
+        public DarkRoast()
+        {
+            Description = "Dark Roast Coffee";
+            Cost = 0.99;
+        }
+    }
+}
---
+++ StarbuzzCoffee-Size/HouseBlend.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Starbuzz_DecoratorPattern
+{
+    public class HouseBlend : Beverage
+    {
+        public HouseBlend()
+        {
+            Description = "House Blend Coffee";
+            Cost = 0.89;
+        }
+    }
+}
